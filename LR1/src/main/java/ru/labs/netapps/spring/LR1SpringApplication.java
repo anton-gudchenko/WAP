26ce106832b25222db6a5e3d5b9194e59e5c0021
SystemFalse <237849999+anton-gudchenko@users.noreply.github.com>
2026-03-20 23:52:18 +0300

    Добавлен класс LR1SpringApplication

---
+++ LR1/src/main/java/ru/labs/netapps/spring/LR1SpringApplication.java
@@ -0,0 +1,11 @@
+package ru.labs.netapps.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LR1SpringApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LR1SpringApplication.class, args);
+    }
+}
